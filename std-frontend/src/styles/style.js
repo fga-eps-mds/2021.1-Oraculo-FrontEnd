09198658b63f8fb1d62f3720ca4713ccf334179c
joao vitor silva <joaovitordmrosa@hotmail.com>
2021-09-02 13:34:17 -0300

    starting project

---
+++ std-frontend/src/styles/style.js
@@ -0,0 +1,52 @@
+export const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  yellow: '#FCDD58',
+  red: '#C94B47',
+
+  blue1: '#BEE0FD',
+  blue2: '#72FCF1',
+  blue3: '#5CA8E6',
+  blue4: '#5289B5',
+  blue5: '#3E7CB0',
+  blue6: '#1F3541',
+
+  gray1: '#DDDDDD',
+  gray2: '#5E6F7D',
+  gray3: '#808C96',
+
+  destructive: '#d21236',
+  destructiveDark: '#781d10',
+  destructiveLlight: '#f49788',
+  success: '#0cc443',
+  successDark: '#04844b',
+  successDarker: '#004a29',
+  warning: '#ffb81a',
+  warningDark: '#765200',
+  brown: '#B65242',
+  brownLight: '#FFC5BC',
+  mustard: '#A57713',
+  mustardLight: '#FFE4A9'
+};
+
+export const fonts = {
+  family: 'montserrat, sans-serif',
+  familySecondary: 'roboto, sans-serif',
+  sizeLg: '16px',
+  sizeMd: '14px',
+  sizeSm: '12px',
+  sizeXs: '10px',
+  sizeH1: '42px',
+  sizeH2: '34px',
+  sizeH3: '28px',
+  sizeH4: '24px',
+  sizeH5: '20px'
+};
+
+export const breakpoints = {
+  mobile: '426px',
+  tablet: '768px',
+  desktop: '980px',
+  wide: '1300px'
+};
+
