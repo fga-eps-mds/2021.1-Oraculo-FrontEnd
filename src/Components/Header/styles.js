fb58ddcfd613f5512bce7b5fde5e1d7920bde357
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-15 23:50:05 -0300

    fga-eps-mds/2021.1-Oraculo#31 New Header Component
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

---
+++ src/Components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Head = styled.div`
+  height: 6.25rem;
+  width: 100%;
+  background-color: #1f3541;
+  display: flex;
+  align-items: center;
+
+  img {
+    cursor: pointer;
+    width: 217px;
+    margin-left: 3rem;
+  }
+`;
