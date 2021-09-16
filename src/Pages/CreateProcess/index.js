e5148f125fd5f170e15052fcf5237af3549bea39
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-15 23:51:59 -0300

    fga-eps-mds/2021.1-Oraculo#31 Add Header component
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/Pages/CreateProcess/index.js
+++ src/Pages/CreateProcess/index.js
@@ -1,29 +1,12 @@
 import React from "react";
-import styled from "styled-components";
+import { StyledTitle } from "./styles";
 import MainButton from "../../Components/MainButton";
-
-const StyledTitle = styled.h1`
-  position: absolute;
-  width: 24.063rem;
-  height: 2.625rem;
-  left: 24rem;
-  top: 9.625rem;
-
-  font-family: Montserrat;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 40px;
-  line-height: 49px;
-  display: flex;
-  align-items: center;
-  text-align: center;
-
-  color: #000000;
-`;
+import Header from "../../Components/Header";
 
 const CreateProcess = () => {
   return (
     <>
+      <Header />
       <StyledTitle>Criar Processo</StyledTitle>
       <MainButton title={"Adicionar Campo"} />
     </>
