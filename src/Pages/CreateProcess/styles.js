694ee4bc124224e8ef632471eb3ca19af675ed64
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-15 23:54:45 -0300

    fga-eps-mds/2021.1-Oraculo#31 styles.js created
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

---
+++ src/Pages/CreateProcess/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const styles = {};
+
+export const StyledTitle = styled.h1`
+  position: absolute;
+  width: 24.063rem;
+  height: 2.625rem;
+  left: 24rem;
+  top: 9.625rem;
+
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 49px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #000000;
+`;
