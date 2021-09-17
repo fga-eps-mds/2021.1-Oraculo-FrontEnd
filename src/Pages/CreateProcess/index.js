17fdcf92e1cba46bf9d00592b5abb6df4755c60f
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-17 09:00:50 -0300

    Page CreateProcess refactored #31
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/Pages/CreateProcess/index.js
+++ src/Pages/CreateProcess/index.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { FaRegFileAlt, FaPlus } from "react-icons/fa";
 
-import { StyledTitle } from "./styles";
-import { StyledBlueRectangle } from "./styles";
-import { StyledPaperSheet } from "./styles";
-import { StyledCut } from "./styles";
-import { StyledWhiteRectangle } from "./styles";
-import { StyledForms } from "./styles";
-import { StyledCancelButton } from "./styles";
-import { StyledCreateButton } from "./styles";
+import {
+  StyledCreateButton,
+  StyledCancelButton,
+  StyledForms,
+  StyledWhiteRectangle,
+  StyledTitle,
+  StyledBlueRectangle,
+  StyledProcess,
+  StyledButtonsDiv,
+} from "./styles";
 
 import MainButton from "../../Components/MainButton";
 import Header from "../../Components/Header";
@@ -16,38 +19,53 @@ const CreateProcess = () => {
   return (
     <>
       <Header />
+      <div>
+        <StyledTitle>
+          <p>Criar Processo</p>
+          <div>
+            <MainButton title={"Adicionar Campo"} />
+          </div>
+        </StyledTitle>
 
-      <StyledTitle>
-        Criar Processo
-        <div>
-          <MainButton title={"Adicionar Campo"} />
-        </div>
-      </StyledTitle>
+        <StyledProcess>
+          <StyledBlueRectangle>
+            <FaRegFileAlt size="20rem" color="white" />
+          </StyledBlueRectangle>
 
-      <StyledBlueRectangle>
-        <StyledPaperSheet>
-          <StyledCut />
-        </StyledPaperSheet>
-      </StyledBlueRectangle>
-
-      <StyledWhiteRectangle>
-        <StyledForms>
-          <form>
-            <h1>Emissor</h1>
-            <input type="text" placeholder="  Emissor" />
-            <h1>N° do SEI</h1>
-            <input type="text" placeholder="  N° do SEI" />
-            <h1>Setor</h1>
-            <input type="text" placeholder="  Setor" />
-            <h1>Data de Emissão</h1>
-            <input type="text" placeholder="  Data de Emissão" />
-            <h1>Tags</h1>
-            <input type="text" placeholder="  +" />
-          </form>
-        </StyledForms>
-        <StyledCancelButton>Cancelar</StyledCancelButton>
-        <StyledCreateButton>Criar</StyledCreateButton>
-      </StyledWhiteRectangle>
+          <StyledWhiteRectangle>
+            <StyledForms>
+              <form>
+                <div>
+                  <h1>Emissor</h1>
+                  <input type="text" placeholder="Emissor" />
+                </div>
+                <div>
+                  <h1>N° do SEI</h1>
+                  <input type="text" placeholder="N° do SEI" />
+                </div>
+                <div>
+                  <h1>Setor</h1>
+                  <input type="text" placeholder="Setor" />
+                </div>
+                <div>
+                  <h1>Data de Emissão</h1>
+                  <input type="text" placeholder="Data de Emissão" />
+                </div>
+                <div>
+                  <h1>Tags</h1>
+                  <button>
+                    <FaPlus />
+                  </button>
+                </div>
+              </form>
+            </StyledForms>
+            <StyledButtonsDiv>
+              <StyledCancelButton>Cancelar</StyledCancelButton>
+              <StyledCreateButton>Criar</StyledCreateButton>
+            </StyledButtonsDiv>
+          </StyledWhiteRectangle>
+        </StyledProcess>
+      </div>
     </>
   );
 };
