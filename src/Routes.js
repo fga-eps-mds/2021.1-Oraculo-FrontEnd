2606a9e1f9a1eb4415b7a9971dd500e3d28568de
jpaulohe4rt <jpaulo.on@gmail.com>
2021-09-19 22:55:00 -0300

    :sparkles: Add route to page 'criar-processo'  #30
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import AdminScreen from "./Pages/AdminScreen/";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from "./Auth/Auth";
 import { history } from "./history";
+import CreateProcess from "./Pages/CreateProcess";
 
 const PrivateRoutes = ({ component: Component, ...prop }) => (
   <Route
@@ -25,6 +26,10 @@ const Routes = () => (
     <Switch>
       <Route exact path="/login" component={() => <LoginScreen />} />
       <PrivateRoutes path="/admin-page" component={() => <AdminScreen />} />
+      <PrivateRoutes
+        path="/criar-processo"
+        component={() => <CreateProcess />}
+      />
       <Route exact path="/" component={() => <LoginScreen />} />
     </Switch>
   </BrowserRouter>
