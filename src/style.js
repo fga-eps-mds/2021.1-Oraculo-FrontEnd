e1b1fe268bc2c67ad7a585db8caa590eda8aba1b
joao vitor silva <joaovitordmrosa@hotmail.com>
2021-09-27 18:19:58 -0300

    fga-eps-mds/2021.1-Oraculo#61 add product styles
    Signed-off-by: joao vitor silva <joaovitordmrosa@hotmail.com>

---
+++ src/style.js
@@ -0,0 +1,21 @@
+export const colors = {
+  navyBlue: '#1F3541',
+  blueGrotto: '#5289B5',
+  whiteSmoke: '#F6F6F6',
+  red: '#FF0000',
+  black: '#000000',
+  white: '#ffffff'
+}
+
+export const fonts = {
+  font: 'Montserrat',
+  sizeXlg: '24px',
+  sizeLg: '18px',
+  sizeMd: '16px',
+  sizeSm: '14px'
+}
+
+export const radius = {
+  sizeMd: '15px',
+  sizeSm: '10px'
+}
