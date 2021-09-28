702585bcef2a294adcc93f93d3f5d8b037a4660e
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-28 14:27:16 -0300

    GenericWhiteButton component created fga-eps-mds/2021.1-Oraculo#85
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>
    Co-authored-by: Victor Rodrigues <victorrsilva17@gmail.com>

---
+++ src/Components/GenericWhiteButton/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledWhiteButton } from "./styles";
+
+const GenericWhiteButton = ({ title }) => (
+  <>
+    <StyledWhiteButton>{title}</StyledWhiteButton>
+  </>
+);
+
+export default GenericWhiteButton;
