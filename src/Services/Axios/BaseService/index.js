78bf38427eaaece15ba142c885a7ede30a6ca435
Lucas Vieira <lucas.engen@outlook.com>
2021-09-29 13:03:41 -0300

    fga-eps-mds/2021.1-Oraculo#77 adds API exports
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Services/Axios/BaseService/index.js
+++ src/Services/Axios/BaseService/index.js
@@ -1,6 +1,3 @@
-let request = axios.post("http://localhost:8000/processos", {
-  seiNumber: seiNumber,
-  emitter: emitter,
-  sector: sector,
-  issueDate: issueDate,
-});
+import { APIProfile, APITags, APIProcess } from "./baseService";
+
+export { APIProfile, APITags, APIProcess };
