13f638bb032495f4713ee676ba31558c9bd056de
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-09-29 22:22:05 -0300

    fga-eps-mds/2021.1-Oraculo#73 fix component DropDownButton
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Components/DropDownButton/index.js
+++ src/Components/DropDownButton/index.js
@@ -5,18 +5,12 @@ const DropDownButton = ({ onChangeOpt }) => {
   return (
     <DivSelectSetor>
       <select onChange={onChangeOpt}>
-        <option name="selectSetorOpt" selected value="criminal">
+        <option selected value="criminal">
           Criminal
         </option>
-        <option name="selectSetorOpt" value="tecnologia">
-          Tecnologia
-        </option>
-        <option name="selectSetorOpt" value="administrativa">
-          Administrativo
-        </option>
-        <option name="selectSetorOpt" value="civil">
-          civil
-        </option>
+        <option value="tecnologia">Tecnologia</option>
+        <option value="administrativa">Administrativo</option>
+        <option value="civil">civil</option>
       </select>
     </DivSelectSetor>
   );
