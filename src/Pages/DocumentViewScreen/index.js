300d631227735696c56c0e82f9f63f8160365571
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-09-30 18:55:28 -0300

    Base to list PocketDocuments created fga-eps-mds/2021.1-Oraculo#74 An extern database is used in this code to test the functionality
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Pages/DocumentViewScreen/index.js
+++ src/Pages/DocumentViewScreen/index.js
@@ -1,18 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../Components/Header";
 import MainButton from "../../Components/MainButton";
 import PocketDocument from "../../Components/PocketDocument";
-import { AiOutlineFilter } from "react-icons/ai";
+import FilterButton from "../../Components/FilterButton";
 import {
   StyledTitle,
   StyledBody,
-  StyledFilterButton,
   StyledOrganizeButtons,
   StyledBigButton,
   StyledSmallButton,
 } from "./styles";
 
+import Posts from "../../Components/Posts/index";
+import Pagination from "../../Components/Pagination/index";
+import axios from "axios";
+
 const DocumentViewScreen = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(4);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
       <Header />
@@ -20,10 +47,7 @@ const DocumentViewScreen = () => {
       <StyledBody>
         <StyledTitle>Processos</StyledTitle>
         <div>
-          <StyledFilterButton>
-            <AiOutlineFilter size="2.8rem" />
-            Filtros
-          </StyledFilterButton>
+          <FilterButton />
           <MainButton title={"Novo Documento"} />
         </div>
         <StyledOrganizeButtons>
@@ -34,12 +58,12 @@ const DocumentViewScreen = () => {
           <StyledBigButton>Tag</StyledBigButton>
           <StyledSmallButton>...</StyledSmallButton>
         </StyledOrganizeButtons>
-        <PocketDocument
-          name="Name"
-          seiNumber="123456789"
-          department="Criminal"
-          documentDate="02/08/6516"
-        ></PocketDocument>
+        <Posts posts={currentPosts} loading={loading} />
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={posts.length}
+          paginate={paginate}
+        />
       </StyledBody>
     </>
   );
