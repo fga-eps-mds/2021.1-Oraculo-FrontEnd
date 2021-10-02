a0dcfec6f65f98964a9120ad321426fe18a9cce6
Lucas Vieira <lucas.engen@outlook.com>
2021-10-01 22:52:58 -0300

    fga-eps-mds/2021.1-Oraculo#77 feat: :sparkles: adds a menu to select department - fixes callback that sends registration HTTP request
    Co-authored-by: Joao Paulo Lima da Silva <jpaulo.on@gmail.com>

--- src/Pages/CreateUser/index.js
+++ src/Pages/CreateUser/index.js
@@ -1,113 +1,114 @@
-import axios from "axios";
 import React, { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { BiUserCircle } from "react-icons/bi";
 import Header from "../../Components/Header";
+import { registerUser } from "../../Services/Axios/profileService";
+
 import {
-  StyledBlueRectangle,
-  StyledButtonsDiv,
-  StyledBackButton,
-  StyledEditButton,
-  StyledForms,
-  StyledViewProfile,
-  StyledWhiteRectangle,
+    StyledBlueRectangle,
+    StyledButtonsDiv,
+    StyledBackButton,
+    StyledEditButton,
+    StyledForms,
+    StyledViewProfile,
+    StyledWhiteRectangle,
 } from "./styles";
 
 const ViewProfile = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [sector, setSector] = useState("");
-  const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [department, setDepartment] = useState(1);
+    const [level] = useState(2);
 
-  async function handleSubmit(event) {
-    console.info(`evento: ${event}`);
+    async function handleSubmit(event) {
+        console.log(`department: ${department}`);
+        const user = {
+            email,
+            password,
+            departmentID: department,
+            level,
+        };
 
-    let request = axios.post("http://localhost:8000/user", {
-      name: name,
-      email: email,
-      sector: sector,
-      password: password,
-    });
+        await registerUser(user, toast);
+    }
 
-    request.then(
-      async (ok) => {
-        toast.success("Usuário criado");
-      },
-      async (error) => {
-        console.error(`failed to send request ${error}`);
-        toast.error("Falha ao cadastrar Usuário");
-      }
+    return (
+        <>
+            <Header />
+            <div>
+                <StyledViewProfile>
+                    <StyledBlueRectangle>
+                        <BiUserCircle size="20rem" color="white" />
+                    </StyledBlueRectangle>
+
+                    <StyledWhiteRectangle>
+                        <StyledForms>
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <h1>Name</h1>
+                                    <input
+                                        id="name"
+                                        type="text"
+                                        placeholder="William Cops"
+                                        value={name}
+                                        onChange={(event) => setName(event.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <h1>Email</h1>
+                                    <input
+                                        id="email"
+                                        type="text"
+                                        placeholder="william@pcgo.org.br"
+                                        value={email}
+                                        onChange={(event) => setEmail(event.target.value)}
+                                    />
+                                </div>
+                                <div>
+                                    <h1>Setor</h1>
+                                    <select
+                                        required
+                                        placeholder="Selecione o departamento"
+                                        onLoad={(event) =>
+                                            setDepartment(event.target.value)
+                                        }
+                                        onChange={(event) =>
+                                            setDepartment(event.target.value)
+                                        }>
+                                        <option>Seção de identificação criminal</option>
+                                        <option>Seção de retrato falado</option>
+                                        <option>Seção de malote</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <h1>Senha</h1>
+                                    <input
+                                        id="password"
+                                        type="password"
+                                        placeholder="Senha"
+                                        onChange={(event) =>
+                                            setPassword(event.target.value)
+                                        }
+                                        value={password}
+                                    />
+                                </div>
+                            </form>
+                        </StyledForms>
+                        <StyledButtonsDiv>
+                            <StyledBackButton onClick={() => window.history.back()}>
+                                Voltar
+                            </StyledBackButton>
+                            <StyledEditButton onClick={handleSubmit}>
+                                Cadastrar
+                            </StyledEditButton>
+                        </StyledButtonsDiv>
+                    </StyledWhiteRectangle>
+                </StyledViewProfile>
+                <Toaster />
+            </div>
+        </>
     );
-  }
-
-  return (
-    <>
-      <Header />
-      <div>
-        <StyledViewProfile>
-          <StyledBlueRectangle>
-            <BiUserCircle size="20rem" color="white" />
-          </StyledBlueRectangle>
-
-          <StyledWhiteRectangle>
-            <StyledForms>
-              <form onSubmit={handleSubmit}>
-                <div>
-                  <h1>Name</h1>
-                  <input
-                    id="name"
-                    type="text"
-                    placeholder="William Cops"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                  />
-                </div>
-                <div>
-                  <h1>Email</h1>
-                  <input
-                    id="email"
-                    type="text"
-                    placeholder="william@pcgo.org.br"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-                </div>
-                <div>
-                  <h1>Setor</h1>
-                  <input
-                    id="sectorNum"
-                    type="text"
-                    placeholder="Setor"
-                    onChange={(event) => setSector(event.target.value)}
-                    value={sector}
-                  />
-                </div>
-                <div>
-                  <h1>Senha</h1>
-                  <input
-                    id="password"
-                    type="text"
-                    placeholder="Senha"
-                    onChange={(event) => setPassword(event.target.value)}
-                    value={password}
-                  />
-                </div>
-              </form>
-            </StyledForms>
-            <StyledButtonsDiv>
-              <StyledBackButton onClick={() => window.history.back()}>
-                Voltar
-              </StyledBackButton>
-              <StyledEditButton onClick={handleSubmit}>
-                Cadastrar
-              </StyledEditButton>
-            </StyledButtonsDiv>
-          </StyledWhiteRectangle>
-        </StyledViewProfile>
-        <Toaster />
-      </div>
-    </>
-  );
 };
 
 export default ViewProfile;
