c13d3a6efa6f885a2831db4a6367168cb19a89ec
Lucas Vieira <lucas.engen@outlook.com>
2021-10-03 00:46:03 -0300

    fga-eps-mds/2021.1-Oraculo#74 refactor: :recycle: fixes code smell

--- src/Components/DropDownButton/index.js
+++ src/Components/DropDownButton/index.js
@@ -1,18 +1,18 @@
-import DivSelectSetor from "./styles";
+import DivSelectSetor from "./DivSelectSetor";
 
 const DropDownButton = ({ onChangeOpt }) => {
-  return (
-    <DivSelectSetor>
-      <select onChange={onChangeOpt}>
-        <option selected value="criminal">
-          Criminal
-        </option>
-        <option value="tecnologia">Tecnologia</option>
-        <option value="administrativa">Administrativo</option>
-        <option value="civil">civil</option>
-      </select>
-    </DivSelectSetor>
-  );
+    return (
+        <DivSelectSetor>
+            <select onChange={onChangeOpt}>
+                <option selected value="criminal">
+                    Criminal
+                </option>
+                <option value="tecnologia">Tecnologia</option>
+                <option value="administrativa">Administrativo</option>
+                <option value="civil">civil</option>
+            </select>
+        </DivSelectSetor>
+    );
 };
 
 export default DropDownButton;
