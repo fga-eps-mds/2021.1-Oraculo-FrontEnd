cc8d36e109da03527df4ab5170b7ef9c697c274e
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-10-06 21:51:05 -0300

    Function to get all process data created fga-eps-mds/2021.1-Oraculo#74
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/Services/Axios/processService.js
+++ src/Services/Axios/processService.js
@@ -1,1 +1,18 @@
+import { APIProcess } from "./BaseService/index";
 
+export async function recieveAllProcess(toast) {
+  try {
+    console.log("Response chamada");
+    const response = await APIProcess.get("/records", {});
+    console.log(`${JSON.stringify(response)}`);
+    return response.data;
+  } catch (err) {
+    const status = err.response?.status;
+
+    if (status === 400) {
+      toast.error("Requisição inválida");
+    } else {
+      toast.error("Não foi possivel realizar a requisição");
+    }
+  }
+}
