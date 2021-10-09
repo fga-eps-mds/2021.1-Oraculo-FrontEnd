88e887fbc42a1bc09291eca8f34bc21107db1a6f
GabrielSSAraujo <gabrielsantos_s96@hotmail.com>
2021-10-09 18:05:28 -0300

    fga-eps-mds/2021.1-Oraculo#73 getting data from the process microservice
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Services/Axios/processService.js
+++ src/Services/Axios/processService.js
@@ -1,1 +1,10 @@
+import { APIProcess } from "./BaseService";
 
+export async function getProcessByID(ID, toast) {
+  try {
+    const response = await APIProcess.get(`/records/${ID}`);
+    return response.data;
+  } catch (error) {
+    toast.error("Erro ao buscar processo!");
+  }
+}
