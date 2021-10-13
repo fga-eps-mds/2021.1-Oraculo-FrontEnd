4f6424f1481533c99ae3ccebd7a82e81df26989c
Lucas Vieira <lucas.engen@outlook.com>
2021-10-12 23:53:31 -0300

    test: changes remote endpoint

--- src/Constants/baseUrls.js
+++ src/Constants/baseUrls.js
@@ -1,12 +1,12 @@
 /**
  * URL para fazer requests ao microsserviço de profiles
  */
-export const BaseUrlProfile = "http://localhost:8000";
+export const BaseUrlProfile = "http://34.83.87.32:8000";
 
 /**
  * URL para fazer requests ao microsserviço de processos
  */
-export const BaseUrlProcess = "http://localhost:8001";
+export const BaseUrlProcess = "http://34.83.87.32:8001";
 
 /**
  * URL para fazer requests ao microsserviço de tags
