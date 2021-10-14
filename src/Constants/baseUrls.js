0642024ce9e8ba6271af565e707b14dbc8ec1914
Lucas Vieira <lucas.engen@outlook.com>
2021-10-14 01:21:10 -0300

    chore: update urls

--- src/Constants/baseUrls.js
+++ src/Constants/baseUrls.js
@@ -1,14 +1,14 @@
 /**
  * URL para fazer requests ao microsserviço de profiles
  */
-export const BaseUrlProfile = "http://137.184.204.215:8000";
+export const BaseUrlProfile = "https://137.184.204.215:8000";
 
 /**
  * URL para fazer requests ao microsserviço de processos
  */
-export const BaseUrlProcess = "http://137.184.204.215:8001";
+export const BaseUrlProcess = "https://137.184.204.215:8001";
 
 /**
  * URL para fazer requests ao microsserviço de tags
  */
-export const BaseUrlTags = "http://137.184.204.215:8002";
+export const BaseUrlTags = "https://137.184.204.215:8002";
