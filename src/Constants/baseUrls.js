5465b6e006e1c1d006059c784a3eaac8c14e08ae
Lucas Vieira <lucas.engen@outlook.com>
2021-10-14 01:42:02 -0300

    chore: removes https

--- src/Constants/baseUrls.js
+++ src/Constants/baseUrls.js
@@ -1,14 +1,14 @@
 /**
  * URL para fazer requests ao microsserviço de profiles
  */
-export const BaseUrlProfile = "https://localhost:8000";
+export const BaseUrlProfile = "http://localhost:8000";
 
 /**
  * URL para fazer requests ao microsserviço de processos
  */
-export const BaseUrlProcess = "https://localhost:8001";
+export const BaseUrlProcess = "http://localhost:8001";
 
 /**
  * URL para fazer requests ao microsserviço de tags
  */
-export const BaseUrlTags = "https://localhost:8002";
+export const BaseUrlTags = "http://localhost:8002";
