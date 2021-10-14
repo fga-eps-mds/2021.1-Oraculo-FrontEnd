10bd308dffb2b095fb2bcc2ca74b4eaafe21531c
Lucas Vieira <lucas.engen@outlook.com>
2021-10-14 00:57:54 -0300

    chore: updates backend endpoints

--- src/Constants/baseUrls.js
+++ src/Constants/baseUrls.js
@@ -1,14 +1,14 @@
 /**
  * URL para fazer requests ao microsserviço de profiles
  */
-export const BaseUrlProfile = "http://34.83.87.32:8000";
+export const BaseUrlProfile = "http://137.184.204.215:8000";
 
 /**
  * URL para fazer requests ao microsserviço de processos
  */
-export const BaseUrlProcess = "http://34.83.87.32:8001";
+export const BaseUrlProcess = "http://137.184.204.215:8001";
 
 /**
  * URL para fazer requests ao microsserviço de tags
  */
-export const BaseUrlTags = "http://localhost:8002";
+export const BaseUrlTags = "http://137.184.204.215:8002";
