9e62843ab49b33337b11550f15dee78b568ad8f0
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-10-16 06:42:11 -0300

    fga-eps-mds/2021.1-Oraculo#73 style: :art: reduces mov block width
    Co-authored-by: Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>

--- src/Components/SectionMovBlock/styles.js
+++ src/Components/SectionMovBlock/styles.js
@@ -2,51 +2,54 @@ import styled from "styled-components";
 import { colors, radius } from "../../style";
 
 const DivNodeBlock = styled.div`
-    display: flex;
-    padding: 0 2rem;
+  display: flex;
+  padding: 0 2rem;
 
-    .content {
-        display: flex;
-        p {
-            margin-left: 2rem;
-        }
-        align-items: center;
+  .content {
+    display: flex;
+    p {
+      margin-left: 2rem;
+      margin-right: 2rem;
     }
+    align-items: center;
+  }
 `;
 
 const StyledLine = styled.div`
-    width: 2px;
-    height: 10rem;
-    background: ${colors.primary};
-    display: flex;
+  width: 2px;
+  height: 10rem;
+  background: ${colors.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  div {
     align-items: center;
     justify-content: center;
-    div {
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        border: 3.5px solid ${colors.primary};
-        width: 1px;
-        height: 1px;
-    }
+    border-radius: 50%;
+    border: 3.5px solid ${colors.primary};
+    width: 1px;
+    height: 1px;
+  }
 `;
 
 const DivMovBlock = styled.div`
-    background: ${colors.white};
-    border-radius: ${radius.sizeMd};
-    border: 1px solid ${colors.black};
-    padding: 0rem 2rem 0.5rem;
-    div {
-        display: flex;
-        align-items: center;
-        svg {
-            margin-right: 1rem;
-            width: 2rem;
-            height: 2rem;
-        }
+  background: ${colors.white};
+  border-radius: ${radius.sizeMd};
+  border: 1px solid ${colors.black};
+  width: 92%;
+  padding: 0rem 2rem 0.5rem;
+  div {
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 1rem;
+      margin-top: 1rem;
+      width: 2rem;
+      height: 2rem;
     }
-    div:last-child {
-        justify-content: space-between;
-    }
+  }
+  div:last-child {
+    justify-content: space-between;
+  }
 `;
 export { DivMovBlock, DivNodeBlock, StyledLine };
