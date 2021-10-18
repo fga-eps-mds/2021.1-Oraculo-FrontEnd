5973bd97c14af048d6586ab401509d06da848d28
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-10-17 23:17:27 -0300

    Registers identifiers created fga-eps-mds/2021.1-Oraculo#76
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
-import { StyledBody } from "./styles";
-
+import SearchBar from "../../Components/SearchBar";
+import {
+  StyledBody,
+  StyledOrganizeButtons,
+  StyledBigButton,
+  StyledFirstSpace,
+} from "./styles";
 import { getProcessTotalNumber } from "../../Services/Axios/processService";
 import toast from "react-hot-toast";
 
@@ -9,6 +14,22 @@ const HomePage = () => {
   return (
     <>
       <HeaderWithButtons />
+      <StyledBody>
+        <h1>Pesquisar Registro</h1>
+        <SearchBar></SearchBar>
+        <h1>Departamento: Não Implementado</h1>
+        <StyledOrganizeButtons>
+          <StyledFirstSpace></StyledFirstSpace>
+          <StyledBigButton>Nº de Registro</StyledBigButton>
+          <StyledBigButton>Cidade</StyledBigButton>
+          <StyledBigButton>UF</StyledBigButton>
+          <StyledBigButton>Solicitante</StyledBigButton>
+          <StyledBigButton>Inclusão</StyledBigButton>
+          <StyledBigButton>Nº do SEI</StyledBigButton>
+          <StyledBigButton>Tags</StyledBigButton>
+          <StyledBigButton>...</StyledBigButton>
+        </StyledOrganizeButtons>
+      </StyledBody>
     </>
   );
 };
--- src/Pages/HomePage/styles.js
+++ src/Pages/HomePage/styles.js
@@ -2,4 +2,42 @@ import styled from "styled-components";
 
 export const styles = {};
 
-export const StyledBody = styled.div``;
+export const StyledBody = styled.div`
+  margin-left: 46px;
+  margin-right: 46px;
+
+  h1 {
+    margin-top: 60px;
+  }
+`;
+
+export const StyledOrganizeButtons = styled.div`
+  background: #1f3541;
+  border: 0px solid #5e5e5e;
+  box-sizing: border-box;
+  border-radius: 10px 10px 0px 0px;
+  height: 4rem;
+  align-items: center;
+  display: flex;
+  justify-items: center;
+`;
+
+export const StyledBigButton = styled.button`
+  width: 12.5%;
+  height: 3rem;
+  align-items: center;
+  justify-content: center;
+  font-family: Montserrat;
+  color: #ffffff;
+  font-style: normal;
+  font-size: 28px;
+  line-height: 20px;
+  text-align: center;
+  background: transparent;
+  cursor: pointer;
+  border: none;
+`;
+
+export const StyledFirstSpace = styled.div`
+  margin-left: 60px;
+`;
