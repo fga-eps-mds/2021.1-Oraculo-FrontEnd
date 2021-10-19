86a9ca3316ae40c7c52eeecf591023d5ba55e49f
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-10-19 11:47:20 -0300

    Register related functions deleted fga-eps-mds/2021.1-Oraculo#76
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,43 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
 import SearchBar from "../../Components/SearchBar";
 import { StyledBody, StyledOrganizeButtons, StyledBigButton } from "./styles";
 
-import Process from "../../Components/Process";
-import Pagination from "../../Components/Pagination/index";
-import {
-  getProcessTotalNumber,
-  getProcessByPage,
-} from "../../Services/Axios/processService";
-import toast from "react-hot-toast";
-
 const HomePage = () => {
-  const [process, setProcess] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [processPerPage] = useState(4);
-  const [allProcesses, setAllProcesses] = useState([]);
-
-  async function setAll() {
-    const temp = await getProcessTotalNumber(toast);
-    setAllProcesses(temp.count);
-    console.log(allProcesses);
-  }
-
-  useEffect(() => {
-    const fetchProcess = async () => {
-      console.log(currentPage);
-      setProcess(await getProcessByPage(currentPage * processPerPage, toast));
-    };
-    fetchProcess();
-  }, [currentPage]);
-
-  window.onload = function () {
-    setAll();
-  };
-
-  // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
   return (
     <>
       <HeaderWithButtons />
@@ -55,12 +21,6 @@ const HomePage = () => {
           <StyledBigButton>Tags</StyledBigButton>
           <StyledBigButton>...</StyledBigButton>
         </StyledOrganizeButtons>
-        <Process process={process} />
-        <Pagination
-          processPerPage={processPerPage}
-          totalProcess={allProcesses}
-          paginate={paginate}
-        />
       </StyledBody>
     </>
   );
