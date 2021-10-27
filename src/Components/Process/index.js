0fbc2b4095b162befe0b37497d15737f40cc759e
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-10-26 22:37:07 -0300

    Design updated for empty fields fga-eps-mds/2021.1-Oraculo#118
    Co-authored-by: Lucas Vieira de Jesus <lucas.engen.cc@gmail.com>
    Co-authored-by: ericoBandeira<ericobandeira@hotmail.com>

--- src/Components/Process/index.js
+++ src/Components/Process/index.js
@@ -8,12 +8,14 @@ const Process = ({ process }) => {
       {process.map((post) => (
         <PocketDocument
           key={post.id}
-          registerNumber={post.register_number}
-          requester={post.requester}
-          inclusionDate={post.document_date}
-          city={post.city}
-          state={post.state}
-          seiNumber={post.sei_number}
+          registerNumber={
+            post.register_number === "" ? "-" : post.register_number
+          }
+          requester={post.requester === "" ? "-" : post.requester}
+          inclusionDate={post.document_date === "" ? "-" : post.document_date}
+          city={post.city === "" ? "-" : post.city}
+          state={post.state === "" ? "-" : post.state}
+          seiNumber={post.sei_number === "" ? "-" : post.sei_number}
         ></PocketDocument>
       ))}
     </StyledListGroup>
