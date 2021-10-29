1c32e7d2af0ea317f91a58bb03f3f81e7df3b96d
jpaulohe4rt <jpaulo.on@gmail.com>
2021-10-29 17:10:50 -0300

    :sparkles: integrated ViewProfile with Profile microservice file fga-eps-mds/2021.1-Oraculo#78
    Co-authored-by: Gabriel Santos Silva Araújos <gabrielsantos_s96@hotmail.com>

--- src/Pages/ViewProfile/index.js
+++ src/Pages/ViewProfile/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { BiUserCircle } from "react-icons/bi";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
-import { getInfoUser, updateUser } from "../../Services/Axios/profileService";
-
+import { getInfoUser, changeUser } from "../../Services/Axios/profileService";
+import { SectionsList } from "../CreateUser/sections";
 import {
   StyledBlueRectangle,
   StyledButtonsDiv,
@@ -14,118 +14,92 @@ import {
   StyledWhiteRectangle,
 } from "./styles";
 
-class ViewProfile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-      loaded: false,
-      newName: "",
-      newSector: "",
-    };
+const ViewProfile = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [sectionID, setSectionID] = useState("");
+
+  async function handleClick(event) {
+    changeUser(toast, name, email, sectionID);
+    const user = await getInfoUser(toast);
+    setName(user.name);
+    setEmail(user.email);
   }
 
-  componentDidMount() {
-    getInfoUser(toast).then((userInfo) => {
-      this.setState({ user: userInfo, loaded: true });
-    });
-  }
+  useEffect(() => {
+    async function fetchUserData() {
+      const user = await getInfoUser(toast);
+      setName(user.name);
+      setEmail(user.email);
+    }
+    fetchUserData();
+  });
 
-  setName(newPass) {
-    this.setState({ password: newPass });
-  }
+  return (
+    <>
+      <HeaderWithButtons />
+      <div>
+        <StyledViewProfile>
+          <StyledBlueRectangle>
+            <BiUserCircle size="20rem" color="white" />
+          </StyledBlueRectangle>
 
-  setEmail(newMail) {
-    this.setState({ email: newMail });
-  }
-
-  setDepartment(newDepartment) {
-    this.setState({ password: newDepartment });
-  }
-
-  async handleClick(event) {
-    const updatedUser = {
-      email: this.state.email,
-      name: this.state.name,
-      department: this.state.department,
-    };
-    await updateUser(updatedUser, toast);
-  }
-
-  render() {
-    const { user } = this.state;
-
-    if (!this.state.loaded) {
-      return (
-        <>
-          <p>Carregando ...</p>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <HeaderWithButtons />
-          <div>
-            <StyledViewProfile>
-              <StyledBlueRectangle>
-                <BiUserCircle size="20rem" color="white" />
-              </StyledBlueRectangle>
-
-              <StyledWhiteRectangle>
-                <StyledForms>
-                  <form>
-                    <div>
-                      <h1>Nome</h1>
-                      <input
-                        id="name"
-                        type="text"
-                        placeholder="William Cops"
-                        defaultValue={user.name}
-                        onChange={(event) => this.setName(event.target.value)}
-                      />
-                    </div>
-                    <div>
-                      <h1>Email</h1>
-                      <input
-                        id="email"
-                        type="text"
-                        placeholder="william@pcgo.org.br"
-                        defaultValue={user.email}
-                        onChange={(event) => this.setEmail(event.target.value)}
-                      />
-                    </div>
-                    <div>
-                      <h1>Departamento</h1>
-                      <input
-                        id="sectionID"
-                        type="text"
-                        placeholder="Setor"
-                        defaultValue={user.sections[0].number}
-                        onChange={(event) => this.setDepartment(event.target.value)}
-                      />
-                    </div>
-                  </form>
-                </StyledForms>
-                <StyledButtonsDiv>
-                  <StyledBackButton onClick={() => window.history.back()}>
-                    Voltar
-                  </StyledBackButton>
-                  <StyledEditButton
-                    onClick={(event) => {
-                      this.handleClick(event);
+          <StyledWhiteRectangle>
+            <StyledForms>
+              <form>
+                <div>
+                  <h1>Nome</h1>
+                  <input
+                    id="name"
+                    type="text"
+                    placeholder="William Cops"
+                    defaultValue={name}
+                    onChange={(event) => setName(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <h1>Email</h1>
+                  <input
+                    id="email"
+                    type="text"
+                    placeholder="william@pcgo.org.br"
+                    defaultValue={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </div>
+                <div>
+                  <h1>Departamento</h1>
+                  <select
+                    required
+                    placeholder="Selecione o departamento"
+                    onChange={(event) => {
+                      setSectionID(event.target.selectedIndex + 1);
                     }}
                   >
-                    Editar
-                  </StyledEditButton>
-                </StyledButtonsDiv>
-              </StyledWhiteRectangle>
-            </StyledViewProfile>
-            <Toaster />
-          </div>
-        </>
-      );
-    }
-  }
-}
+                    <SectionsList />
+                  </select>
+                </div>
+              </form>
+            </StyledForms>
+            <StyledButtonsDiv>
+              <StyledBackButton onClick={() => window.history.back()}>
+                Voltar
+              </StyledBackButton>
+              <StyledEditButton
+                onClick={(event) => {
+                  handleClick(event);
+                }}
+                type="submit"
+              >
+                Editar
+              </StyledEditButton>
+            </StyledButtonsDiv>
+          </StyledWhiteRectangle>
+        </StyledViewProfile>
+        <Toaster />
+      </div>
+    </>
+  );
+};
 
 export default ViewProfile;
