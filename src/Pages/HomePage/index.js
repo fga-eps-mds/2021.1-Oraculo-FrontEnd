d9779f045426c21f0f199048432a8047316ba814
Victor Rodrigues <victorrsilva17@gmail.com>
2021-10-29 22:51:45 -0300

    Showing Section on HomePage fga-eps-mds/2021.1-Oraculo#116

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
 import SearchBar from "../../Components/SearchBar";
+import { getInfoUser } from "../../Services/Axios/profileService";
 import { StyledBody, StyledOrganizeButtons, StyledBigButton } from "./styles";
 
 const HomePage = () => {
+  const [section, setSection] = useState("");
+
+  useEffect(() => {
+    async function fetchUserData() {
+      const user = await getInfoUser();
+      console.log("User Data", user);
+      setSection(user.sections[0].name);
+      return user;
+    }
+
+    console.log("Departamento:", section);
+    fetchUserData();
+  });
+
   return (
     <>
       <HeaderWithButtons />
       <StyledBody>
         <h1>Pesquisar Registro</h1>
         <SearchBar></SearchBar>
-        <h1>Departamento: Não Implementado</h1>
+        <h1>Departamento: {section}</h1>
         <StyledOrganizeButtons>
           <StyledBigButton>Nº de Registro</StyledBigButton>
           <StyledBigButton>Cidade</StyledBigButton>
