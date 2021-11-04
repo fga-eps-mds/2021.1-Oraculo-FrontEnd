94b972d698247907e1c6c5e01c8344a5604e3964
Max <maxhb.df@gmail.com>
2021-11-04 14:26:35 -0300

    Add Department Component fga-eps-mds/2021.1-Oraculo#147
    Co-authored-by: Joao Vitor  <joaovitordmrosa@hotmail.com>"

---
+++ src/Components/Departments/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PocketDocument from "../PocketDocument";
+import { StyledListGroup } from "./style";
+
+const Departments = ({ departments }) => {
+  const seiNumberLimit = (seiNumber) => {
+    if (seiNumber.length < 10) {
+      return seiNumber;
+    } else {
+      return `${seiNumber.substring(0, 10)}...`;
+    }
+  };
+
+  return (
+    <StyledListGroup>
+      {departments.map((post) => (
+        <PocketDocument
+          key={post.id}
+          registerNumber={
+            post.register_number === "" ? "-" : post.register_number
+          }
+          requester={post.requester === "" ? "-" : post.requester}
+          inclusionDate={post.document_date === "" ? "-" : post.document_date}
+          city={post.city === "" ? "-" : post.city}
+          state={post.state === "" ? "-" : post.state}
+          seiNumber={
+            post.sei_number === "" ? "-" : seiNumberLimit(post.sei_number)
+          }
+        ></PocketDocument>
+      ))}
+    </StyledListGroup>
+  );
+};
+
+export default Departments;
