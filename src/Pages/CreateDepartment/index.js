d5f39fc9709ed5d87c90208133705c3f2036003a
Victor Rodrigues <victorrsilva17@gmail.com>
2021-11-08 02:16:21 -0300

    Remove quotation marks from Departments fga-eps-mds/2021.1-Oraculo#177

--- src/Pages/CreateDepartment/index.js
+++ src/Pages/CreateDepartment/index.js
@@ -41,7 +41,7 @@ const CreateDepartment = () => {
                     id="email"
                     type="text"
                     required
-                    placeholder="´Nome do departamento"
+                    placeholder="Nome do departamento"
                     value={department}
                     onChange={(event) => setDepartment(event.target.value)}
                   />
