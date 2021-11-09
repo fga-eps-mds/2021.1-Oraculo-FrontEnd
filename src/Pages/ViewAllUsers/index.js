89301ba76f79873228a085599e625aade1e04c29
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-11-08 21:37:49 -0300

    Unused imports of SearchBar removed fga-eps-mds/2021.1-Oraculo#195
    Co-authored-by: Victor Rodrigues <victorrsilva17@gmail.com>

--- src/Pages/ViewAllUsers/index.js
+++ src/Pages/ViewAllUsers/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
 import MainButton from "../../Components/MainButton";
-import SearchBar from "../../Components/SearchBar";
 import toast, { Toaster } from "react-hot-toast";
 import { history } from "../../history";
 import {
