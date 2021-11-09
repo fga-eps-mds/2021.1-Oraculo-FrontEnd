1b4be934081bdec1794ef191d135a8ed3b5d9806
AntonioIgorCarvalho <antonioigorcarvalho@gmail.com>
2021-11-09 20:16:37 -0300

    New page created to editDepartments fga-eps-mds/2021.1-Oraculo#187
    Co-authored-by: Victor Rodrigues <victorrsilva17@gmail.com>

---
+++ src/Pages/EditDepartment/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { BiBuildings } from "react-icons/bi";
+import HeaderWithButtons from "../../Components/HeaderWithButtons";
+import { registerDepartment } from "../../Services/Axios/profileService";
+
+import {
+  StyledBlueRectangle,
+  StyledButtonsDiv,
+  StyledBackButton,
+  StyledRegisterButton,
+  StyledForms,
+  StyledViewProfile,
+  StyledWhiteRectangle,
+} from "../CreateDepartment/styles.js";
+import { useParams } from "react-router";
+
+const EditDepartment = () => {
+  const { id } = useParams();
+  const [department, setDepartment] = useState("");
+
+  async function handleClick(event) {
+    return registerDepartment(department, toast);
+  }
+
+  return (
+    <>
+      <HeaderWithButtons />
+      <div>
+        <Toaster />
+        <StyledViewProfile>
+          {/* Create Department image */}
+          <StyledBlueRectangle>
+            <BiBuildings size="20rem" color="white" />
+          </StyledBlueRectangle>
+
+          <StyledWhiteRectangle>
+            <StyledForms>
+              <form>
+                <div>
+                  <h1>Editar Departamento</h1>
+                  <input
+                    id="email"
+                    type="text"
+                    required
+                    placeholder="Nome do departamento"
+                    value={department}
+                    onChange={(event) => setDepartment(event.target.value)}
+                  />
+                </div>
+              </form>
+            </StyledForms>
+            <StyledButtonsDiv>
+              <StyledBackButton
+                type="button"
+                onClick={() => window.history.back()}
+              >
+                Voltar
+              </StyledBackButton>
+              <StyledRegisterButton
+                type="button"
+                onClick={(event) => handleClick(event)}
+              >
+                Editar
+              </StyledRegisterButton>
+            </StyledButtonsDiv>
+          </StyledWhiteRectangle>
+        </StyledViewProfile>
+      </div>
+    </>
+  );
+};
+
+export default EditDepartment;
