7d0a7ea811dde85b995c1b6673ae34b28465b601
Victor Rodrigues <victorrsilva17@gmail.com>
2021-11-09 21:50:20 -0300

    Create function to verify if user token is valid fga-eps-mds/2021.1-Oraculo#197
    Co-authored-by: Henrike100 <henrikem100@gmail.com>

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,3 +1,6 @@
+import jwt_decode from "jwt-decode";
+import { getToken } from "../Services/Axios/profileService";
+
 export const STORAGE_KEY = "user-token";
 
 const isAuthenticated = () => !!localStorage.getItem(STORAGE_KEY);
@@ -6,4 +9,19 @@ const login = (token) => localStorage.setItem(STORAGE_KEY, token);
 
 const logout = () => localStorage.removeItem(STORAGE_KEY);
 
-export { isAuthenticated, login, logout };
+const tokenCheck = () => {
+  if (isAuthenticated()) {
+    const token = getToken();
+    const decoded = jwt_decode(token);
+    let expirationTime = new Date(decoded.exp * 1000);
+    const now = new Date();
+    if (expirationTime < now) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+
+export { isAuthenticated, login, logout, tokenCheck };
