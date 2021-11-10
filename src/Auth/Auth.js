fb71cb42ab106b4a8bacadec76ffb06e5946a73f
Victor Rodrigues <victorrsilva17@gmail.com>
2021-11-09 21:53:48 -0300

    Adding comments fga-eps-mds/2021.1-Oraculo#197
    Co-authored-by: Henrike100 <henrikem100@gmail.com>

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -9,12 +9,19 @@ const login = (token) => localStorage.setItem(STORAGE_KEY, token);
 
 const logout = () => localStorage.removeItem(STORAGE_KEY);
 
+// Check if User token on local storage is valid
 const tokenCheck = () => {
   if (isAuthenticated()) {
     const token = getToken();
+
+    // Decode token
     const decoded = jwt_decode(token);
+
+    // Gets the expiration date of the token
     let expirationTime = new Date(decoded.exp * 1000);
     const now = new Date();
+
+    // Checks if token is expired
     if (expirationTime < now) {
       return false;
     } else {
