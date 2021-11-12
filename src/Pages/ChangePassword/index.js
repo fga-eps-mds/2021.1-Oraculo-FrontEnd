c7d64122e5a79fcd107adecfdf0c6fffb74888df
Victor Rodrigues <victorrsilva17@gmail.com>
2021-11-12 01:14:43 -0300

    Adding mask to change pswrd screen fga-eps-mds/2021.1-Oraculo#227
    Co-authored-by: Henrike100 <henrikem100@gmail.com>

--- src/Pages/ChangePassword/index.js
+++ src/Pages/ChangePassword/index.js
@@ -3,10 +3,13 @@ import toast, { Toaster } from "react-hot-toast";
 import HeaderWithButtons from "../../Components/HeaderWithButtons";
 import { FormLogin, StyledDiv } from "./styles";
 import { changeUserPassword } from "../../Services/Axios/profileService";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const ChangePassword = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [isRevealPassword, setIsRevealPassword] = useState(false);
+  const [isRevealConfirmPassword, setIsRevealConfirmPassword] = useState(false);
 
   async function handleClick(event) {
     password === confirmPassword
@@ -26,22 +29,52 @@ const ChangePassword = () => {
             <h1>Alterar Senha</h1>
 
             <h2>Nova senha:</h2>
-            <input
-              type="text"
-              required
-              placeholder="Nova senha"
-              onChange={(event) => setPassword(event.target.value)}
-              value={password}
-            />
+            <div className="input-div">
+              <input
+                type={isRevealPassword ? "text" : "password"}
+                required
+                placeholder="Nova senha"
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
+              />
+              {isRevealPassword ? (
+                <FaRegEye
+                  className="input-icon"
+                  onClick={() => setIsRevealPassword(!isRevealPassword)}
+                />
+              ) : (
+                <FaRegEyeSlash
+                  className="input-icon"
+                  onClick={() => setIsRevealPassword(!isRevealPassword)}
+                />
+              )}
+            </div>
 
             <h2>Confirme a senha:</h2>
-            <input
-              type="text"
-              required
-              placeholder="Confirme a senha"
-              onChange={(event) => setConfirmPassword(event.target.value)}
-              value={confirmPassword}
-            />
+            <div className="input-div">
+              <input
+                type={isRevealConfirmPassword ? "text" : "password"}
+                required
+                placeholder="Confirme a senha"
+                onChange={(event) => setConfirmPassword(event.target.value)}
+                value={confirmPassword}
+              />
+              {isRevealConfirmPassword ? (
+                <FaRegEye
+                  className="input-icon"
+                  onClick={() =>
+                    setIsRevealConfirmPassword(!isRevealConfirmPassword)
+                  }
+                />
+              ) : (
+                <FaRegEyeSlash
+                  className="input-icon"
+                  onClick={() =>
+                    setIsRevealConfirmPassword(!isRevealConfirmPassword)
+                  }
+                />
+              )}
+            </div>
 
             <button class="save-button" type="submit">
               Salvar
