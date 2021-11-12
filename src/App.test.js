c7afbfc6ad3a52f3d126ca2989a8379164f88c40
Lucas Vieira <lucas.engen@outlook.com>
2021-11-12 05:18:52 -0300

    test: :white_check_mark: adds mainbutton components to test suite

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import { render } from "@testing-library/react";
 import App from "./App";
+import MainButton from "./Components/MainButton";
 
 test("renders main app component", () => {
   render(<App />);
+});
+
+test("renders main button component", () => {
+  render(<MainButton/>
 });
