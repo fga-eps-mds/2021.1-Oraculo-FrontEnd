e61fb7de7ea032b98ac17589230c8e692fd0b7e9
Lucas Vieira <lucas.engen@outlook.com>
2021-11-12 05:34:23 -0300

    test: adds unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 import MainButton from "./Components/MainButton";
+import HeaderWithButtons from "./Components/HeaderWithButtons";
 
 test("renders main app component", () => {
   render(<App />);
@@ -8,4 +9,8 @@ test("renders main app component", () => {
 
 test("renders main button component", () => {
   render(<MainButton />);
+});
+
+test("renders header with buttons component", () => {
+  render(<HeaderWithButtons/>);
 });
