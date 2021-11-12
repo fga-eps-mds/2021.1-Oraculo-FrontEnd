a1c7cf1e83d2649dc5e3a0bf69a80620645e3606
jpaulohe4rt <jpaulo.on@gmail.com>
2021-11-12 03:31:30 -0300

    :sparkles: add style in component PocketTags fga-eps-mds/2021.1-Oraculo#225
    Co-authored-by: ericoBandeira<ericobandeira@hotmail.com>
    Co-authored-by: Max Henrique <maxhb.df@gmail.com>

--- src/Components/PocketTags/styles.js
+++ src/Components/PocketTags/styles.js
@@ -19,9 +19,27 @@ export const StyledText = styled.text`
   font-family: Montserrat;
   font-style: normal;
   font-size: 26px;
+  flex: 1;
 `;
 
 export const StyledListGroup = styled.ul`
   display: contents;
   list-style-type: none;
+  align-items: center;
+`;
+
+export const StyledCircleDiv = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+`;
+
+export const CircleColor = styled.div`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+  border: 1px solid #000000;
+  align-items: center;
 `;
